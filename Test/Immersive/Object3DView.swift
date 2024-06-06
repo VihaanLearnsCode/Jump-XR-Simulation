a4035d0effad4fef2203d920681d1a50f2be11cc
VihaanLearnsCode <vihaanrawarkhare@gmail.com>
2024-06-06 17:06:23 -0500

    Immersive 3D models implemented

---
+++ Test/Immersive/Object3DView.swift
@@ -0,0 +1,18 @@
+//
+//  Object3DView.swift
+//  Test
+//
+//  Created by Vihaan Khare on 6/6/24.
+//
+
+import SwiftUI
+
+struct Object3DView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Object3DView()
+}
