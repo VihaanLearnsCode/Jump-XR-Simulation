ecf666aea486362bc5c3eb050396f250ce2f68c5
VihaanLearnsCode <vihaanrawarkhare@gmail.com>
2024-06-07 14:04:28 -0500

    Created animated toolbar

---
+++ Test/Immersive/VirtualEnvironmentView.swift
@@ -0,0 +1,18 @@
+//
+//  VirtualEnvironmentView.swift
+//  Test
+//
+//  Created by Vihaan Khare on 6/7/24.
+//
+
+import SwiftUI
+
+struct VirtualEnvironmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VirtualEnvironmentView()
+}
---
+++ Test/Model/ExpandableButton.swift
@@ -0,0 +1,8 @@
+//
+//  ExpandableButton.swift
+//  Test
+//
+//  Created by Vihaan Khare on 6/7/24.
+//
+
+import Foundation
